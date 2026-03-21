4bea77131351f4a4ff39bfc2e3121344d38d7172
Mqseal <30034267@cc.peralta.edu>
2026-03-21 22:07:10 +0000

    markov

--- markov.cpp
+++ markov.cpp
@@ -25,11 +25,6 @@ int readWordsFromFile(string filename, string words[], int maxWords)
 {
     ifstream inputFile(filename);
 
-    if (!inputFile.is_open())
-    {
-        return -1;
-    }
-
     int count = 0;
 
     while (count < maxWords && inputFile >> words[count])
@@ -64,108 +59,10 @@ int buildMarkovChain(const string words[], int numWords, int order,
 
 string getRandomSuffix(const string prefixes[], const string suffixes[],
                        int chainSize, string currentPrefix)
-{
-    int matchCount = 0;
 
-    for (int i = 0; i < chainSize; i++)
-    {
-        if (prefixes[i] == currentPrefix)
-        {
-            matchCount++;
-        }
-    }
-
-    if (matchCount == 0)
-    {
-        return "";
-    }
-
-    int pick = rand() % matchCount;
-    int currentMatch = 0;
-
-    for (int i = 0; i < chainSize; i++)
-    {
-        if (prefixes[i] == currentPrefix)
-        {
-            if (currentMatch == pick)
-            {
-                return suffixes[i];
-            }
-
-            currentMatch++;
-        }
-    }
-
-    return "";
-}
 
 string getRandomPrefix(const string prefixes[], int chainSize)
-{
-    if (chainSize <= 0)
-    {
-        return "";
-    }
 
-    int index = rand() % chainSize;
-    return prefixes[index];
-}
 
 string generateText(const string prefixes[], const string suffixes[],
                     int chainSize, int order, int numWords)
-{
-    if (chainSize <= 0 || order <= 0 || numWords <= 0)
-    {
-        return "";
-    }
-
-    string currentPrefix = getRandomPrefix(prefixes, chainSize);
-
-    if (currentPrefix == "")
-    {
-        return "";
-    }
-
-    string result = currentPrefix;
-
-    string currentWords[10];
-    int wordIndex = 0;
-    string temp = "";
-
-    for (int i = 0; i < currentPrefix.length(); i++)
-    {
-        if (currentPrefix[i] == ' ')
-        {
-            currentWords[wordIndex] = temp;
-            wordIndex++;
-            temp = "";
-        }
-        else
-        {
-            temp += currentPrefix[i];
-        }
-    }
-
-    currentWords[wordIndex] = temp;
-
-    for (int i = 0; i < numWords - order; i++)
-    {
-        string newWord = getRandomSuffix(prefixes, suffixes, chainSize, currentPrefix);
-
-        if (newWord == "")
-        {
-            break;
-        }
-
-        result += " " + newWord;
-
-        for (int j = 0; j < order - 1; j++)
-        {
-            currentWords[j] = currentWords[j + 1];
-        }
-
-        currentWords[order - 1] = newWord;
-        currentPrefix = joinWords(currentWords, 0, order);
-    }
-
-    return result;
-}
