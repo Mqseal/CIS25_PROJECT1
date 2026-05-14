28128b736b50147031f4b72f3fa1b9fb639f60f6
Mqseal <30034267@cc.peralta.edu>
2026-05-13 23:10:27 -0700

    Complete main program for Markov generator

--- main.cpp
+++ main.cpp
@@ -19,6 +19,49 @@ int main()
     cout << "Enter order (1, 2, or 3): ";
     cin >> order;
 
+    cout << "Enter number of words to generate: ";
+    cin >> numWords;
+
+    if (order < 1 || order > 3)
+    {
+        cout << "Invalid order. Use 1, 2, or 3." << endl;
+        return 1;
+    }
+
+    const int MAX_WORDS = 100000;
+    const int MAX_CHAIN = 100000;
+
+    string words[MAX_WORDS];
+    string prefixes[MAX_CHAIN];
+    string suffixes[MAX_CHAIN];
+
+    int wordCount = readWordsFromFile(filename, words, MAX_WORDS);
+
+    if (wordCount == -1)
+    {
+        cout << "Could not open file." << endl;
+        return 1;
+    }
+
+    if (wordCount <= order)
+    {
+        cout << "Not enough words in the file." << endl;
+        return 1;
+    }
+
+    int chainSize = buildMarkovChain(words, wordCount, order, prefixes, suffixes, MAX_CHAIN);
+
+    if (chainSize <= 0)
+    {
+        cout << "Chain is empty." << endl;
+        return 1;
+    }
+
+    string output = generateText(prefixes, suffixes, chainSize, order, numWords);
+
+    cout << endl;
+    cout << "Generated text:" << endl;
+    cout << output << endl;
 
     return 0;
 }
